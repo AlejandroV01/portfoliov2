049c65f6a88b31657dc552116c587ef84318da86
Alejandro <alexvera0109@gmail.com>
2023-06-01 18:22:34 -0400

    favicon and title

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,11 @@
 import './global.css'
-
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <title>Alejandro Vera</title>
+        <link rel='icon' href='/AV.ico' />
+      </head>
       <body suppressHydrationWarning={true}>
         <main>{children}</main>
       </body>
